f4d12aa903ae826226d2f7a7df5b8ff1cfe9585b
Manuri-Rasara <manurirasarahewage@gmail.com>
2026-05-10 10:14:15 +0530

    work component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import FlowArtDefaultDemo from "@/myComponents/FlowArtDefaultDemo";
+import Work from "@/myComponents/Work";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <FlowArtDefaultDemo />
+      <Work />
     </div>
   );
 }
---
+++ myComponents/Work.tsx
@@ -0,0 +1,9 @@
+import { ProjectShowcase } from "../components/project-showcase";
+
+export default function Work() {
+  return (
+    <main className="min-h-screen bg-background flex items-center justify-center w-full px-6 py-16">
+      <ProjectShowcase />
+    </main>
+  )
+}
