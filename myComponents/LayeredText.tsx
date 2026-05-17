4057dcd4112c9cc33fb05b5ebb52f9b2fd8be1b5
Manuri-Rasara <manurirasarahewage@gmail.com>
2026-05-17 15:18:14 +0530

    remove Layered Text

--- myComponents/LayeredText.tsx
+++
@@ -1,9 +0,0 @@
-import { LayeredText } from "../components/layered-text";
-
-export default function LayeredTextDemo() {
-  return (
-    <div className=" bg-white dark:bg-black flex items-center justify-center">
-      <LayeredText />
-    </div>
-  )
-}
