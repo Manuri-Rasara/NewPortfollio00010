d781451acce4a933747ea373ca8d219e54caf586
Manuri-Rasara <manurirasarahewage@gmail.com>
2026-08-01 00:07:18 +0530

    remove shadow in Whats app Icon

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ const DemoOne = () => {
           href="https://wa.me/94787531134?text=Hello%20Manuri%2C%20I%E2%80%99m%20interested%20in%20your%20design%20and%20development%20services.%20I%E2%80%99d%20love%20to%20connect%20and%20discuss%20a%20project."
           target="_blank"
           rel="noreferrer"
-          className="fixed bottom-6 right-6 z-[9999] flex items-center justify-center rounded-full shadow-lg hover:scale-110 transition-transform duration-300"
+          className="fixed bottom-6 right-6 z-[9999] flex items-center justify-center rounded-full  hover:scale-110 transition-transform duration-300"
           aria-label="Open WhatsApp chat"
         >
            <Image
